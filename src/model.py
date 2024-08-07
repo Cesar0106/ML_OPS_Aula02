88c138bb34c130f16d432258a8d7f3424f3d58f8
Cesar0106 <62568082+Cesar0106@users.noreply.github.com>
2024-08-07 10:45:57 -0300

    Tudo

--- src/model.py
+++ src/model.py
@@ -1,11 +1,11 @@
 import pickle
 
 def load_model():
-    with open('API/models/model.pkl', 'rb') as file:
+    with open(r'C:\Users\cesar\OneDrive\Desktop\Insper\Eletivas\ml_ops\Aula2\models\model.pkl', 'rb') as file:
         model = pickle.load(file)
     return model
 
 def load_encoder():
-    with open('API/models/ohe.pkl', 'rb') as file:
+    with open(r'C:\Users\cesar\OneDrive\Desktop\Insper\Eletivas\ml_ops\Aula2\models\model.pkl', 'rb') as file:
         encoder = pickle.load(file)
     return encoder
